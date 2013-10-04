1c71ad91851b965b63b28f38b7fc7c5d9f8dd6df
YottaSecond <ovaltinevortex@gmail.com>
2013-10-03 23:24:59 -0400

    still not clear on whether using a union to hold all effect data is a good idea... might take a different route

--- include/Effects.h
+++ include/Effects.h
@@ -2,27 +2,51 @@
 #ifndef EFFECTS_H
 #define EFFECTS_H
 
+typedef float SAMPLE;
+
+typedef enum EffectType
+{
+	EMPTY,
+	DELAY,
+} EffectType;
+
+typedef struct DelayData
+{
+	float* delayBuffer;
+	int delayTimeInMs;
+	int delayTimeInSamples;
+	float feedback;
+} DelayData;
+
 typedef union EffectData
 {
-	
+	DelayData;
 } EffectData;
 
+//A ChainLink contains a function pointer to an effect function, and data for that effect to use
+//Each Chain will refer to a list of ChainLinks - its serial list of effects
+//The first ChainLink in the list will be passed to the audio callback, which will increment through each ChainLink in the list
 typedef struct ChainLink
 {
 	//function pointer to audio effect function
+	EffectType effectType;
 	void(*effectFunction)();
 	EffectData effectData;
 	struct ChainLink* nextLink;
+	//we want to pass this data to the callback
+	int numInputChannels, numOutputChannels;
 	//should include pointer to data (fx parameters - union type?)
 } ChainLink;
 
-
-typedef struct Chain //kinda want to rename this to ChainLink but I'm already confused...
+//A Chain is an audio stream. It contains a pointer to a ChainLink, which
+//contains effect data and is passed to the audio callback for that stream.
+//Chain is implemented as a list; multiple chains allows for parallel audio processes
+typedef struct Chain
 {
 	ChainLink chainLink;
 	struct Chain* nextChain;
 	PaStreamParameters inputParameters, outputParameters;
-	PaStream *stream;
+	PaStream* stream;
 } Chain;
 
 
